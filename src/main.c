a127ecc5656ffe32f0196f2a00e139bb11e6e003
OleksandrG <ger_1987@ukr.ner>
2019-07-30 09:40:57 +0300

    - Added USB HID - Added ADC mesurement

--- src/main.c
+++ src/main.c
@@ -1,44 +1,104 @@
-/*
-**
-**                           Main.c
-**
-**
-**********************************************************************/
-/*
-   Last committed:     $Revision: 00 $
-   Last changed by:    $Author: $
-   Last changed date:  $Date:  $
-   ID:                 $Id:  $
-
-**********************************************************************/
 #include "stdint.h"
+#include "stdbool.h"
+#include "string.h"
 
 #include "stm32f10x_conf.h"
 
 #include "servoControl.h"
+#include "measurement.h"
+#include "serviceFunction.h"
 
-uint32_t sysTime = 0;
+#include "usbHIDInterface.h"
+#include "usb_user_setings.h"
 
-void sysTimeProcessing(void) {
-    sysTime++;
+#define ADC_REZ_SIZE  (56 * 2)
+#define EP_N           1
+uint16_t adcRezBuff[ADC_REZ_SIZE];
+
+MesPeriod timeAdc;
+
+
+bool TX_BUSY_F = false;
+bool RX_F = false;
+bool TX_F = false;
+
+typedef enum {
+    SEND_DATA,
+    RUN_CLOCKWISE,
+    RUN_COUNTERCLOCKWISE,
+    STOP,
+} COMMAND_LIST;
+
+
+#pragma pack(push, 1)
+#define PROTOCOL_BUFF_SIZE ((64 - 4 - 4) / 2)
+typedef struct SendDataCommandDesc{
+    uint32_t headr;
+    uint32_t size;
+    uint16_t buff[PROTOCOL_BUFF_SIZE];
+} PSendDataCommand;
+
+typedef union {
+    PSendDataCommand command;
+    uint8_t buff[sizeof(PSendDataCommand)];
+} SendDataCommand ;
+#pragma pack(pop)
+
+SendDataCommand dataCommand = {
+    .command = {
+        .headr = SEND_DATA,
+    }
+};
+
+void getAdcRez(uint16_t rez[])
+{
+    addTimeMes(&timeAdc);
+    /**copy Data to USB Buffer*/
+    if(TX_F) {
+        return;
+    }
+    TX_F = true;
+    dataCommand.command.size = sizeof(dataCommand.buff);
+    memcpy(dataCommand.command.buff, rez, sizeof(dataCommand.command.buff));
 }
 
-uint32_t updateTime = 100;
-uint16_t pwmVal     = 0;
+void usbHIDRxCB(uint8_t epNumber, uint8_t numRx, uint8_t* rxData)
+{
+}
+
+void usbHIDTxCompleteCB(void)
+{
+    TX_BUSY_F = false;
+}
+
+
+void txCompleteCB(void)
+{
+    TX_BUSY_F = false;
+}
 
 int main(void)
 {
-    servoControlInit(sysTimeProcessing);
-    servoControlStart(20, CLOCKWISE);
+
+    usbHIDInit();
+    measurementInit(getAdcRez, adcRezBuff, ADC_REZ_SIZE);
+    usbHIDAddRxCB(usbHIDRxCB);
+    usbHIDAddTxCompleteCB(txCompleteCB);
+
+    //initSysTic();
+    measurementStart();
+    //servoControlStart(20, CLOCKWISE);
     while(1)
     {
-        if(sysTime > updateTime) {
-            updateTime = sysTime + 25;
-            pwmVal += 100;
-            if(pwmVal >= 1100) {
-                pwmVal = 0;
+        if(RX_F) {
+
+        };
+        if(TX_F) {
+            if( usbHIDEPIsReadyToTx(EP_01) ) {
+                usbHIDTx(EP_01, dataCommand.buff, sizeof(SendDataCommand));
             }
-            servoControlSetSpeed(pwmVal);
+            TX_F      = false;
+            TX_BUSY_F = true;
         }
     }
 }
---
+++ src/serviceFunction.h
@@ -0,0 +1,17 @@
+#ifndef __SERVIS_FUNCTION_H__
+#define __SERVIS_FUNCTION_H__
+
+#include "stdint.h"
+
+#define REZ_CNT 200
+typedef struct MesPeriod{
+    uint32_t timeMes[REZ_CNT];
+    uint32_t prevTime;
+    uint32_t k;
+} MesPeriod;
+
+void     initSysTic(void);
+uint32_t getSysTime(void);
+void     addTimeMes(MesPeriod *mesPeriod);
+
+#endif
